a53e6f85af0ea656d56c3bcc81120ad4654cbe95
aixgl@sina.com <aixgl@sina.com>
2020-12-17 21:16:38 +0800

    add code

--- basic.magic/ch03/code3.1.go
+++ basic.magic/ch03/code3.1.go
@@ -11,9 +11,11 @@ import (
     "math"
 )
 
+// 长度大转长度小
 func int64toint32() {
+    fmt.Println("=======int64toint32========")
 	//转换 长度从大到小
-    // 超过32位
+    // 超过32位，转换后是0值。
 	num64 := int64(math.Pow(2, 38))
 	num32 := int32(num64)
     // output: num64:=274877906944, num32:=0
@@ -24,5 +26,18 @@ func int64toint32() {
 	num32 = int32(num64)
     // output: num64:=1024, num32:=1024
 	fmt.Printf("num64:=%d, num32:=%d\n", num64, num32)
+}
+
+// 同长度不同类型名 也要转成相同的
+func int32toint() {
+    fmt.Println("=======int32toint========")
+    num := 32
+	num2 := int32(2)
+    
+    /* 编译报错：invalid operation: num * num2 (mismatched types int and int32)*/
+    // fmt.Println(num * num2) // int 和 int32 的类型不匹配
+
+    // output:64 //可以正确执行
+	fmt.Println(num * int(num2))  
 }
 

98b1358305632cf63fd9a688cc177d814874e6e9
aixgl@sina.com <aixgl@sina.com>
2021-03-23 18:34:56 +0800

    fix

--- basic.magic/ch09/code_main.go
+++ basic.magic/ch09/code_main.go
@@ -11,4 +11,5 @@ import (
 
 func main() {
     forRountine()
+    channelWaitGoroutine()
 }
--- basic.magic/ch09/routine.go
+++ basic.magic/ch09/routine.go
@@ -14,5 +14,20 @@ func forRountine() {
     for i := 0; i < 10; i++ {
         go printer(i)
     }
-    time.Sleep(2 * time.Second)
+    time.Sleep(5 * time.Second)
+}
+
+func channelWaitGoroutine(){
+    fmt.Println("--------channelWaitGoroutine--------")
+    done := make(chan struct{})
+    go func() {
+        sum := 0
+        for i:=0; i < 100; i++ {
+            sum += i
+        }
+        log.Println("done", sum)
+        done <- struct{}{} // signal the main goroutine
+    }()
+    // 阻塞：wait for background goroutine to finish    
+    <-done 
 }
